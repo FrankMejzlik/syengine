e0a100c52584f8c4323a2324414fc4c99f5cca05
devwesp <devwesp@gmail.com>
2019-03-16 14:48:50 +0100

    Add Utils.h file with Utils namespace and ReadTextFile() function.

---
+++ SYEngine/Helper/Utils.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+#include "common.h"
+
+namespace SYE
+{
+
+namespace Utils
+{
+  std::string ReadTextFile(std::string_view filepath)
+  {
+    std::string content;
+    std::fstream file(filepath.data(), std::ios::in);
+
+    // If opening file failed
+    if (!file.is_open())
+    {
+      DLog(eLogType::Error, "Failed to open file %s.", filepath);
+      return std::string();
+    }
+
+    // Init empty line
+    std::string line("");
+
+    while (!file.eof())
+    {
+      std::getline(file, line);
+      content.append(line + '\n');
+    }
+
+    file.close();
+
+    return content;
+  }
+
+} // namespace Utils
+
+} // namespace SYE
+
+
